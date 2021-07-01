dbd1edd371103a9ee6387665aab9177188c12d1b
Deepak Singh <deepak.singh@biz2credit.org>
2021-07-01 15:49:15 +0530

    fe question3 solution added

--- MONTHLY TEST/FE/QUESTION 3/my-app/src/App.js
+++ MONTHLY TEST/FE/QUESTION 3/my-app/src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import {Modal, Button} from 'react-bootstrap'
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+  const [currentTask, setCurrentTask] = useState({})
+
+  const [show, setShow] = useState(false);
+
+
+  const addTaskHandler = (e) => {
+    e.preventDefault()
+    if (task === "") {
+      alert("Please enter task")
+      return
+    }
+    const id = uuidv4()
+    setTasks([...tasks, { id, task }])
+    setTask("")
+  }
+
+  const editHandler = (id)=>{
+    setShow(true)
+    const curr_task = tasks.find(t=>t.id === id)
+    setCurrentTask(curr_task)
+  }
+
+  const saveTask = (e)=>{
+    e.preventDefault()
+    const index = tasks.findIndex(t=>t.id === currentTask.id)
+    const t = tasks
+    t.splice(index,1, {...currentTask})
+    setTasks(t)
+    setShow(false)
+  }
+
+  const deleteHandler = (id)=>{
+    const filteredData = tasks.filter(t=>t.id !== id)
+    setTasks([...filteredData])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <Modal show={show} onHide={()=>setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={saveTask}>
+            <input type="text" placeholder="edit task.." value={currentTask.task} onChange={(e) => setCurrentTask({...currentTask,task:e.target.value})} />
+            <Button variant="primary" type="submit">
+            Save Changes
+          </Button>
+          </form>
+        </Modal.Body>
+      </Modal>
+      <div className="App">
+        <form onSubmit={addTaskHandler}>
+          <input type="text" placeholder="task .." value={task} onChange={(e) => setTask(e.target.value)} />
+          <button type="submit">Add</button>
+        </form>
+        <div>
+          {tasks.map(t =>
+            <>
+              <h3>{t.task}</h3>
+              <button onClick={()=>editHandler(t.id)}>Edit</button>
+              <button onClick={()=>deleteHandler(t.id)}>Delete</button>
+            </>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
